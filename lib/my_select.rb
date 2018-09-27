601b75bbae3f35509251fda9bbfad8100960261e
Roberto Samia <bojosteph@gmail.com>
2018-09-27 22:48:38 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,15 +1,17 @@
 
- def my_select(array)
-  
-  array.each_with_index do |n, i|
-      array[i] = code.call(n)
-    end
-  
-array_1 = []
-
-
-even = Proc.new do |n|
-  array_1.select{ |num|  num.even? }
-  
-end
+ def my_select(array) 
+  i =0 
+  select = [] 
+  while i < array.length 
+    if (yield(array[i]))
     
+      select << array[i] 
+    end 
+    i += 1 
+  end 
+  select 
+end 
+
+my_select(nums) do |num| 
+  num.even? 
+end 
