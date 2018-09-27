6691ae3573a41247bd185f85fc9b12d6c326c787
Roberto Samia <bojosteph@gmail.com>
2018-09-27 22:49:23 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,17 +1,2 @@
 
- def my_select(array) 
-  i =0 
-  select = [] 
-  while i < array.length 
-    if (yield(array[i]))
-    
-      select << array[i] 
-    end 
-    i += 1 
-  end 
-  select 
-end 
-
-my_select(nums) do |num| 
-  num.even? 
-end 
+ 
