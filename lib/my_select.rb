3a70844748240c90987470af1ff2541a1576f664
Roberto Samia <bojosteph@gmail.com>
2018-09-27 22:45:26 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,12 +4,14 @@
   array.each_with_index do |n, i|
       array[i] = code.call(n)
     end
-  end
-end
+  
+
 
 array_1 = []
 
 
 even = Proc.new do |n|
   array_1.select{ |num|  num.even? }
+  
+end
     
