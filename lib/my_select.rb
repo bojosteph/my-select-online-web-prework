431157c63214d3bce6dc4319fcc5c480ab54e7a1
Roberto Samia <bojosteph@gmail.com>
2018-09-27 22:41:40 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,5 +1,15 @@
 
-# def my_select(array)
-   
+ def my_select(array)
+  
+  array.each_with_index do |n, i|
+      self[i] = code.call(n)
+    end
+  end
+end
 
+array_1 = [1, 2, 3, 4]
+array_2 = [2, 3, 4, 5]
+
+square = Proc.new do |n|
+  n ** 2
     
